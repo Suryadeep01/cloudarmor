0d3eda6834df1ca9813cf91d2bf1cfb879bfbd9a
suryadeep <suryadeep984@gmail.com>
2025-01-05 22:33:55 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/cloudarmor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cloudarmor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WebViewApp(),
+    );
+  }
+}
+
+class WebViewApp extends StatefulWidget {
+  @override
+  _WebViewAppState createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    // For Android, you need to enable hybrid composition.
+    // Optional: Uncomment if you face rendering issues on older devices.
+    // if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cloud Armor'),
+        backgroundColor: Colors.blue,
+      ),
+      body: WebViewWidget(
+        controller: WebViewController()
+          ..setJavaScriptMode(JavaScriptMode.unrestricted)
+          ..loadRequest(Uri.parse('https://veirdo.in/')),
+      ),
+    );
+  }
+}
